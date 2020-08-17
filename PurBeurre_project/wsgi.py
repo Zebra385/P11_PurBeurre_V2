39e59e485550b12506bc47bea67f808e739fb99d
houche <houcheserge@gmail.com>
2020-08-17 09:30:22 +0200

    refondation dossiers

--- PurBeurre_project/wsgi.py
+++ PurBeurre_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PurBeurre_project.PurBeurre_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PurBeurre_project.settings')
 
 application = get_wsgi_application()
