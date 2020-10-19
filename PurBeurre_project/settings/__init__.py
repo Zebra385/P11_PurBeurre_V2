4006aee03f9756cb48c8465cd56df0630aefd1b8
houche <houcheserge@gmail.com>
2020-10-19 09:57:31 +0200

    Empty table Attributs

--- PurBeurre_project/settings/__init__.py
+++ PurBeurre_project/settings/__init__.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
     'store.apps.StoreConfig',
     'substituts.apps.SubstitutsConfig',
     ]
